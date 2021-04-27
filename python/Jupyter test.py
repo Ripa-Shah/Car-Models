0fe8530b4b6880b24ea87be6ff174584a979c762
Ripa-Shah <70597931+Ripa-Shah@users.noreply.github.com>
2021-04-26 21:18:15 -0700

    Create Jupyter test.py

---
+++ python/Jupyter test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import pandas as pd
+
+
+# In[1]:
+
+
+x=5
+print(x)
+
+
+# In[2]:
+
+
+x=10
+y=7
+print(x*y)
+
+
+# In[3]:
+
+
+def doubleMe(x):
+    return 2*x
+
+
+# In[4]:
+
+
+print(doubleMe(5))
+
+
+# In[7]:
+
+
+print(doubleMe(z))
+
+
+# In[ ]:
+
+
+print(doubleMe(x))
+
+
+# In[6]:
+
+
+z=12
+
+
+# In[9]:
+
+
+df= pd.read_csv("c:/test/python3/out.csv")
+
+
+# In[10]:
+
+
+df.head(10)
+
+
+# In[ ]:
+
+
+
+
